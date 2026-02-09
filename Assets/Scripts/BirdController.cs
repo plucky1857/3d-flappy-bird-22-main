4bf5eaf2c1ae6e85e845ef652c2e2127307fad6c
plucky1857 <pluckypenguin1@gmail.com>
2026-02-09 16:47:27 +0100

    initial repository commit

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BirdController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    private float speed = 3;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void OnJump(InputValue jumpValue)
+    {
+        Vector3 jumpreset = rb.linearVelocity;
+        jumpreset.y = 0f;
+        rb.linearVelocity = jumpreset;
+
+        Vector3 jumpPower = new Vector3(0.0f, 3f, 0.0f);
+
+        rb.AddForce(jumpPower, ForceMode.Impulse);
+    }
+    void Update()
+    {
+
+    }
+
+        void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, movementY, 0.0f);
+        rb.AddForce(movement * speed, ForceMode.Impulse);
+    }
+}
